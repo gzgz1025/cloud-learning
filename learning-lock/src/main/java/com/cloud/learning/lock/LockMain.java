9b5b6499c0cdb6b1f96304f15c2b0f49ba9fbe57
gzgz1025 <pzl688pzl>
2021-01-12 17:00:44 +0800

    乐观锁与悲观锁

---
+++ learning-lock/src/main/java/com/cloud/learning/lock/LockMain.java
@@ -0,0 +1,18 @@
+package com.cloud.learning.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: LockMain
+ * @Description:
+ * @Author: pzl
+ * @CreateDate: 2021/1/12 16:59
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class LockMain {
+    public static void main(String[] args) {
+        SpringApplication.run(LockMain.class,args);
+    }
+}
